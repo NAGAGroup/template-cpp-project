15612e7b14282b7a4c0f46efc71408c367397b9e
Jack Myers <jackhyers97@gmail.com>
2024-11-09 16:58:14 -0800

    better activation
    Signed-off-by: Jack Myers <jackhyers97@gmail.com>

--- activation/linux.sh
+++ activation/linux.sh
@@ -8,5 +8,6 @@ if [ -z "$PIXI_LINUX_ACTIVE" ]; then
   export CUDA_ROOT="$CONDA_PREFIX/targets/x86_64-linux"
   export CUDA_LIB_PATH="$CUDA_ROOT/lib/stubs:$CONDA_PREFIX/lib"
   export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$CUDA_LIB_PATH:$LD_LIBRARY_PATH"
+  export CMAKE_TOOLCHAIN_FILE="$PIXI_PROJECT_ROOT/toolchans/linux.cmake"
   export PIXI_LINUX_ACTIVE="true"
 fi
