88216dbdfffc2284bb0e8ffb74274d18f02934f3
Jack Myers <jackhyers97@gmail.com>
2024-12-08 20:02:01 -0800

    WIP
    Signed-off-by: Jack Myers <jackhyers97@gmail.com>

--- files/conda-activate.sh
+++ files/conda-activate.sh
@@ -3,4 +3,4 @@
 set -e
 
 install_dir=$(bash -c "cd -- $(dirname -- "${BASH_SOURCE[0]}") &>/dev/null && pwd")
-find "$install_dir" -maxdepth 1 -exec cp -r {} "$BUILD_PREFIX" ";"
+find "$install_dir" -maxdepth 1 -exec cp -r {} "$CONDA_PREFIX" ";"
